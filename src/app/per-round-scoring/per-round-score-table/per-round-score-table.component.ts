595d676ff069e9c5eb754b5bea097173312d7215
Dave Cloutier <dcloutier@intertech.com>
2019-10-22 14:28:31 -0500

    Cleanup of errors during build with production flag set to true

--- src/app/per-round-scoring/per-round-score-table/per-round-score-table.component.ts
+++ src/app/per-round-scoring/per-round-score-table/per-round-score-table.component.ts
@@ -4,23 +4,24 @@ import { Player } from '@models/player';
 import { NumberModalComponent, NumberModalData } from '@util/number-modal/number-modal.component';
 import { PerRoundScoringService } from '../per-round-scoring.service';
 
+const ROUND_CUTOFF = 10;
+
 @Component({
   selector: 'st-per-round-score-table',
   templateUrl: './per-round-score-table.component.html',
   styleUrls: [ './per-round-score-table.component.scss' ]
 })
 export class PerRoundScoreTableComponent {
-  public roundCutoff = 10;
   public showAllRounds = false;
 
-  constructor(private gameService: PerRoundScoringService, private modalService: ModalService) {}
+  constructor(public gameService: PerRoundScoringService, private modalService: ModalService) {}
 
   trimmedTable(): boolean {
-    return this.gameService.roundList.length > this.roundCutoff;
+    return this.gameService.roundList.length > ROUND_CUTOFF;
   }
 
-  minRoundToShow(): number {
-    return this.showAllRounds ? 0 : Math.max(this.gameService.roundList.length - this.roundCutoff, 0);
+  showRound(roundIndex: number): boolean {
+    return this.showAllRounds || roundIndex >= (this.gameService.roundList.length - ROUND_CUTOFF);
   }
 
   editScore(player: Player, round: number): void {
