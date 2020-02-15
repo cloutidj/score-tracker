8cbec566e8a1989da9ffe02c93a6e369bc556e1f
cloutidj <cloutidj@gmail.com>
2020-02-15 09:40:46 -0600

    Clarify text on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'st-home',
-  template: `
-      <div class="card-columns">
-          <a class="card clickable" [routerLink]="['/perRoundScoring']">
-              <div class="card-block">
-                  <div class="card-title">
-                      Scoring Per Round Game
-                  </div>
-                  <div class="card-text">
-                      Each player scores per round - like Qwirkle
-                  </div>
-              </div>
-          </a>
-      </div>
-  `
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {}
